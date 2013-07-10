f42fd504b79066be70d99bc7194c1d815c08fe3a
William <flyingdragontooth@gmail.com>
2013-07-10 11:30:53 +0800

    Moved AsyncTask.HTTP.pas to upper directory

--- demo/AsyncTaskDemo.dpr
+++ demo/AsyncTaskDemo.dpr
@@ -4,12 +4,12 @@ uses
   FMX.Forms,
   AsynctaskDemoForm in 'AsynctaskDemoForm.pas' {Form2},
   AsyncTask in '..\AsyncTask.pas',
-  AsyncTask.HTTP in 'AsyncTask.HTTP.pas';
+  AsyncTask.HTTP in '..\AsyncTask.HTTP.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm2, Form2);
-  Application.Run;
+  AApplication.CreateForm(TForm2, Form2);
+  pplication.Run;
 end.
